9e300f3ddd9016ad8fa17e888f980ff53fe7b673
Dylan Rumph <dprumph@gmail.com>
2017-09-09 14:05:39 -0500

    Initial upload of web files

---
+++ quote.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="project1.css">
+</head>
+
+<body>
+
+<h1>Dylan Rumph</h1>
+
+<img src="images/profile.png" alt="Profile Photo">
+
+<p>My first paragraph.</p>
+
+</body>
+</html>
