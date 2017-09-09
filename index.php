5b9559a3457cc0064e1f58ca7a66c866c325d4ec
Dylan Rumph <dprumph@gmail.com>
2017-09-09 15:16:42 -0500

    added favicon and html title

--- index.php
+++ index.php
@@ -2,13 +2,16 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="project1.css">
+<title>DWA 15 Project 1</title>
 </head>
 
 <body>
 
 <h1>Dylan Rumph</h1>
 
+<div>
 <img src="images/profile.png" alt="Profile Photo">
+</div>
 
 <p>My first paragraph.</p>
 
