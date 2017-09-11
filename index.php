f85ed793472a16909cf541aff1b9e6ed5c83967c
Dylan Rumph <dprumph@gmail.com>
2017-09-11 18:34:50 -0500

    Corrected spelling errors

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 <h2>About Me</h2>
 
 <p>I have been working as an RF engineer but would like to transition into the software
-engineering space thus am taking this class and working towards a Masters degree.</p>
+engineering space thus am taking this class and working towards a Master's degree.</p>
 
 <h2> Words of Wisdom </h2>
 
@@ -35,10 +35,19 @@ $quotes = [
     6 => 'Dreaming, after all, is a form of planning. – Gloria Steinem',
     7 => 'Either write something worth reading or do something worth writing. – Benjamin Franklin',
     8 => 'Education costs money.  But then so does ignorance. – Sir Claus Moser',
-    9 => 'Nothing is impossible, the word itself says, “I’m possible!” – Audrey Hepburn'
+    9 => 'Nothing is impossible, the word itself says, “I’m possible!” – Audrey Hepburn',
+    10 => 'He who has a why to live can bear almost any how. – Friedrich Nietzsche',
+    11 => 'He that respects himself is safe from others. – Henry Wadsworth Longfellow',
+    12 => 'Common sense is genius dressed in its working clothes. – Ralph Waldo Emerson',
+    13 => 'In three words I can sum up everything I’ve learned about life: it goes on. – Robert Frost',
+    14 => 'Don’t take life too seriously. You’ll never get out of it alive. – Elbert Hubbard',
+    15 => 'Nothing that you have not given away will ever be really yours. – C. S. Lewis',
+    16 => 'Life is what happens to you while youre busy making other plans. – Allen Saunders',
+    17 => 'Good sense, disciplined by experience and inspired by goodness, issues in practical wisdom. – Samuel Smiles ',
+    18 => 'One of the greatest pieces of economic wisdom is to know what you do not know. – John Kenneth Gilbraith'
 ];
 
-echo $quotes[rand(0,9)];
+echo $quotes[rand(0,18)];
  ?>
  </h3>
 
